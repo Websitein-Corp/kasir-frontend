c47b8861401b0298f3bdc690ca1e4c013648eb65
Darren Lionardo <darrenlionardo@gmail.com>
2024-09-01 12:24:19 +0700

    Refactor Main files

---
+++ src/components/SideNavigation.vue
@@ -0,0 +1,10 @@
+<template>
+  <nav class="w-full bg-primary-900">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
