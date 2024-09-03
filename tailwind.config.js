dc3820840edc837729020241b93a7ba3b86ec296
Darren Lionardo <darrenlionardo@gmail.com>
2024-09-04 00:04:25 +0700

    Adjust tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,24 +2,25 @@
 module.exports = {
   content: ["./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          50: "#F0FDF9",
+          100: "#CDFAEE",
+          200: "#9AF5DD",
+          300: "#48E5C2",
+          400: "#2FD2B3",
+          500: "#16B699",
+          600: "#0F927E",
+          700: "#107566",
+          800: "#125D53",
+          900: "#144D45",
+          950: "#052E2B",
+        },
+      },
+    },
     fontFamily: {
       helvetica: ["Helvetica"],
-    },
-    colors: {
-      primary: {
-        50: "#F0FDF9",
-        100: "#CDFAEE",
-        200: "#9AF5DD",
-        300: "#48E5C2",
-        400: "#2FD2B3",
-        500: "#16B699",
-        600: "#0F927E",
-        700: "#107566",
-        800: "#125D53",
-        900: "#144D45",
-        950: "#052E2B",
-      },
     },
   },
   plugins: [],
