958351d5db25d08324f0db85769f08d2f4b10dd6
Darren Lionardo <darrenlionardo@gmail.com>
2024-09-06 01:28:12 +0700

    Add pinia store for modal

---
+++ src/stores/useModal.js
@@ -0,0 +1,21 @@
+import { defineStore } from "pinia";
+
+export default defineStore("modal", {
+  state: () => ({
+    isOpened: false,
+    title: "",
+    icon: null,
+    body: null,
+  }),
+  actions: {
+    open() {
+      this.isOpened = true;
+    },
+    close() {
+      this.isOpened = false;
+    },
+    toggle() {
+      this.isOpened = !this.isOpened;
+    },
+  },
+});
