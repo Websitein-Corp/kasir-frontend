71662703f3fafc424fbda4bf9cb1f5b7e1c9f877
Darren Lionardo <darrenlionardo@gmail.com>
2024-09-29 21:49:31 +0700

    Add Datetime input component

---
+++ src/components/Input/DatetimeInput.vue
@@ -0,0 +1,21 @@
+<template>
+  <VueDatePicker
+    class="border-2 border-primary-700 rounded"
+    v-model="date"
+    range
+    :disabled="disabled"
+  />
+</template>
+
+<script setup>
+import VueDatePicker from "@vuepic/vue-datepicker";
+
+defineProps({
+  disabled: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const date = defineModel(new Date());
+</script>
