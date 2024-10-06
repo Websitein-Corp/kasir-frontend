4d158616b73e753cc60227c6345b8b908c0a3680
Darren Lionardo <darrenlionardo@gmail.com>
2024-10-06 20:51:56 +0700

    Remove dummy transaction data

--- src/stores/useTable.js
+++ src/stores/useTable.js
@@ -2,20 +2,7 @@ import { defineStore } from "pinia";
 
 export default defineStore("table", {
   state: () => ({
-    items: [
-      {
-        invoiceNumber: "tokotok_001",
-        trDatetime: "2024-08-19 10:00:00",
-        totalPrice: "Rp200.000",
-        status: "SUCCESS",
-      },
-      {
-        invoiceNumber: "tokotok_002",
-        trDatetime: "2024-08-19 10:00:00",
-        totalPrice: "Rp200.000",
-        status: "SUCCESS",
-      },
-    ],
+    items: [],
     filters: {
       keyword: "",
       date: [
