888dee87deda10aa719208fe7516e27c79c29232
Darren Lionardo <darrenlionardo@gmail.com>
2024-10-13 00:04:46 +0700

    Update useAuth

--- src/stores/useAuth.js
+++ src/stores/useAuth.js
@@ -1,22 +1,53 @@
 import { defineStore } from "pinia";
 import router from "@/router";
+import axios from "axios";
 
 export default defineStore("auth", {
   state: () => ({
     authToken: localStorage.getItem("auth_token"),
     shopId: localStorage.getItem("shop_id"),
+    isAuthenticated: false,
   }),
 
   actions: {
-    async checkLoginSession() {
-      if (this.authToken) {
-        if (this.shopId) {
-          await router.push("/home");
-        } else {
-          await router.push("/shop");
+    async checkLoginSession(route) {
+      try {
+        const response = await axios.get(
+          `${process.env.VUE_APP_API_BASE_URL}/api/shop-list`,
+          {
+            headers: {
+              Authorization: `Bearer ${this.authToken}`,
+            },
+            withCredentials: true,
+          }
+        );
+
+        this.isAuthenticated = true;
+
+        if (
+          route.path === "/login" ||
+          (this.shopId && route.path === "/shop")
+        ) {
+          router.push("/");
         }
+      } catch (response) {
+        this.handleUnauthenticated(response);
+      }
+    },
+
+    handleUnauthenticated(response) {
+      if (response.status === 401) {
+        router.push("/login");
       } else {
-        await router.push("/login");
+        if (this.authToken) {
+          if (this.shopId) {
+            router.push("/");
+          } else {
+            router.push("/shop");
+          }
+        } else {
+          router.push("/login");
+        }
       }
     },
 
@@ -28,6 +59,11 @@ export default defineStore("auth", {
     setShopId(id) {
       this.shopId = id;
       localStorage.setItem("shop_id", id);
+    },
+
+    clearLocalStorage() {
+      localStorage.removeItem("auth_token");
+      localStorage.removeItem("shop_id");
     },
   },
 });
