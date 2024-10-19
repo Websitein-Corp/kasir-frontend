cc357fb4abc0673d1bda010bb596521651112439
darren <darren@mobilepulsa.com>
2024-10-20 00:19:21 +0700

    Add additional attr for useIngredient

--- src/stores/useIngredient.js
+++ src/stores/useIngredient.js
@@ -3,8 +3,11 @@ import { defineStore } from "pinia";
 export default defineStore("ingredient", {
   state: () => ({
     sku: "",
+    all: [],
     list: [],
     used: [],
+    updatedAll: [],
+    updated: [],
     filters: {
       keyword: "",
     },
