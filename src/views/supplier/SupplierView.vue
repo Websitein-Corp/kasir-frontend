ec7527af389177a7b243b153e406184437985b7c
Darren <darrenlionardo@gmail.com>
2024-11-11 23:33:22 +0700

    Fix merge conflict

--- src/views/supplier/SupplierView.vue
+++ src/views/supplier/SupplierView.vue
@@ -8,70 +8,65 @@
     />
   </div>
   <div v-else-if="auth.isAuthenticated">
-    <DataTable :column-count="6">
-      <template v-slot:action-2>
-        <DatetimeInput
-          v-model="table.filters.date"
-          :max-date="new Date()"
-          range
-        ></DatetimeInput>
-        <SearchInput class="mx-4" v-model="table.filters.keyword"></SearchInput>
-        <div class="flex gap-5">
-          <DashboardButton @click="handleAddSupplier"
-            >Add Supplier</DashboardButton
-          >
-        </div>
-      </template>
-      <template v-slot:thead>
-        <tr>
-          <th>ID</th>
-          <th>Status</th>
-          <th>Transaction Date</th>
-          <th>Due Date</th>
-          <th>Total Price</th>
-          <th>Actions</th>
-        </tr>
-      </template>
-      <template v-slot:tbody>
-        <tr v-for="(item, index) in table.items" :key="index">
-          <td>{{ item.id }}</td>
-          <td>
-            <span v-if="item.status === 'NOT_PAID'">Belum Dibayar</span>
-            <span v-if="item.status === 'PAID'">Sudah Dibayar</span>
-          </td>
-          <td>{{ item.tr_datetime }}</td>
-          <!-- Use tr_datetime field -->
-          <td>{{ item.due_date }}</td>
-          <!-- Use due_date field -->
-          <td>{{ item.total_price }}</td>
-          <!-- Use total_price field -->
-          <td>
-            <div class="flex flex-row space-x-4">
-              <CustomButton
-                size="full"
-                height="lg"
-                iconSide="right"
-                label="EDIT"
-                align="center"
-                :icon="mdiContentSave"
-                @click="handleEditSupplier(item)"
-                class="bg-primary-600"
-              />
-              <CustomButton
-                size="full"
-                height="lg"
-                iconSide="right"
-                label="DELETE"
-                align="center"
-                :icon="mdiDelete"
-                @click="deleteItem(item, index)"
-                class="bg-red-600"
-              />
-            </div>
-          </td>
-        </tr>
-      </template>
-    </DataTable>
+    <PageContainer title="Supplier" subtitle="Daftar supplier yang ada">
+      <DataTable :column-count="6">
+        <template v-slot:action-2>
+          <DatetimeInput
+            v-model="table.filters.date"
+            :max-date="new Date()"
+            range
+          ></DatetimeInput>
+          <SearchInput
+            class="mx-4"
+            v-model="table.filters.keyword"
+          ></SearchInput>
+          <div class="flex gap-5">
+            <DashboardButton @click="handleAddSupplier"
+              >Add Supplier</DashboardButton
+            >
+          </div>
+        </template>
+        <template v-slot:thead>
+          <tr>
+            <th>Nama Supplier</th>
+            <th>Nomor Telepon</th>
+            <th>Alamat</th>
+            <th>Actions</th>
+          </tr>
+        </template>
+        <template v-slot:tbody>
+          <tr v-for="(item, index) in table.items" :key="index">
+            <td>{{ item.name }}</td>
+            <td>{{ item.phone_number }}</td>
+            <td>{{ item.address }}</td>
+            <td>
+              <div class="flex flex-row space-x-4">
+                <CustomButton
+                  size="full"
+                  height="lg"
+                  iconSide="right"
+                  label="EDIT"
+                  align="center"
+                  :icon="mdiContentSave"
+                  @click="handleEditSupplier(item)"
+                  class="bg-primary-600"
+                />
+                <CustomButton
+                  size="full"
+                  height="lg"
+                  iconSide="right"
+                  label="DELETE"
+                  align="center"
+                  :icon="mdiDelete"
+                  @click="deleteItem(item, index)"
+                  class="bg-red-600"
+                />
+              </div>
+            </td>
+          </tr>
+        </template>
+      </DataTable>
+    </PageContainer>
   </div>
   <div v-else>
     <DefaultSkeleton class="mb-2" />
@@ -89,6 +84,7 @@ import SearchInput from "@/components/Input/SearchInput.vue";
 import CustomButton from "@/components/Button/CustomButton.vue";
 import DashboardButton from "@/components/Button/DashboardButton.vue";
 import SupplierFormView from "./SupplierFormView.vue";
+import PageContainer from "@/views/PageContainer.vue";
 import axios from "axios";
 import useTable from "@/stores/useTable";
 import useToast from "@/stores/useToast";
@@ -99,9 +95,10 @@ import { useRoute } from "vue-router";
 const auth = useAuth();
 const table = useTable();
 const toast = useToast();
-const route = useRoute();
 const isShowingForm = ref(false);
 const selectedSupplier = ref(null);
+const route = useRoute();
+const basedOn = ref("SUPPLY_DATE");
 
 let debounce;
 
@@ -135,7 +132,7 @@ const fetchSupplier = async () => {
   );
 
   const { data } = await axios.get(
-    `${process.env.VUE_APP_API_BASE_URL}/api/supplier/supply/list?shop_id=${auth.shopId}&date_start=${formattedDate[0]}&date_end=${formattedDate[1]}&keyword=${table.filters.keyword}&page=${table.page.current}`,
+    `${process.env.VUE_APP_API_BASE_URL}/api/supplier?shop_id=${auth.shopId}&date_start=${formattedDate[0]}&date_end=${formattedDate[1]}&keyword=${table.filters.keyword}&page=${table.page.current}`,
     {
       headers: {
         Authorization: `Bearer ${auth.authToken}`,
@@ -147,10 +144,9 @@ const fetchSupplier = async () => {
   table.items = data.data.map((item) => {
     return {
       id: item.id,
-      status: item.status,
-      tr_datetime: item.tr_datetime,
-      due_date: item.due_date,
-      total_price: item.total_price,
+      name: item.name,
+      phone_number: item.phone_number,
+      address: item.address,
     };
   });
 
@@ -166,7 +162,6 @@ const handleAddSupplier = () => {
   isShowingForm.value = true;
 };
 
-// Function to handle edit supplier action
 const handleEditSupplier = (item) => {
   selectedSupplier.value = { ...item };
   isShowingForm.value = true;
@@ -178,9 +173,15 @@ const handleCancel = () => {
 
 const deleteItem = async (item, index) => {
   try {
+    const requestBody = {
+      shop_id: auth.shopId,
+      name: item.name,
+    };
+
     await axios.delete(
-      `${process.env.VUE_APP_API_BASE_URL}/api/ingredients/${item.id}`,
+      `${process.env.VUE_APP_API_BASE_URL}/api/supplier?shop_id=${auth.shopId}`,
       {
+        data: requestBody,
         headers: {
           Authorization: `Bearer ${localStorage.getItem("auth_token")}`,
         },
