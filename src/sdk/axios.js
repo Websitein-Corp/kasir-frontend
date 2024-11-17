6172f9555cd699679d1a48d22fd5be35274c4971
Darren <darrenlionardo@gmail.com>
2024-11-17 17:56:14 +0700

    Add axios interceptors

---
+++ src/sdk/axios.js
@@ -0,0 +1,48 @@
+import axios from "axios";
+import useAuth from "@/stores/useAuth";
+import useToast from "@/stores/useToast";
+import usePage from "@/stores/usePage";
+
+const axiosInstance = axios.create();
+
+axiosInstance.interceptors.response.use(
+  function (response) {
+    // if code 2xx
+    const page = usePage();
+    page.loading = false;
+
+    return response;
+  },
+  function (error) {
+    const auth = useAuth();
+    const page = usePage();
+    const toast = useToast();
+
+    page.loading = false;
+
+    // if code other than 2xx
+    if (error.response.status === 401) {
+      auth.handleUnauthenticated();
+      return new Promise(() => {});
+    } else if (error.response.status % 500 === 1) {
+      // if code is 5xx
+      toast.message = "Gagal";
+      toast.description = "Terjadi kesalahan, mohon coba lagi";
+      toast.type = "FAILED";
+      toast.trigger();
+
+      auth.handleAxiosError();
+      return new Promise(() => {});
+    } else {
+      toast.message = "Gagal";
+      toast.description = error.response?.data.message || error.message;
+      toast.type = "FAILED";
+      toast.trigger();
+
+      auth.handleAxiosError();
+      return new Promise(() => {});
+    }
+  }
+);
+
+export { axiosInstance as axios };
