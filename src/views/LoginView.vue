3ba574f15d078de819902b08f9f38a1864a0d36f
Darren <darrenlionardo@gmail.com>
2024-12-21 18:16:11 +0700

    Adjust svg attribute

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -2,9 +2,6 @@
   <div class="relative w-screen h-screen overflow-hidden">
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      version="1.1"
-      xmlns:xlink="http://www.w3.org/1999/xlink"
-      xmlns:svgjs="http://svgjs.dev/svgjs"
       width="100%"
       height="100%"
       preserveAspectRatio="none"
