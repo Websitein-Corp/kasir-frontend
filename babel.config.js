c295c4011d1dc23e5eacde1e488df197c7b08cda
Darren <darrenlionardo@gmail.com>
2025-01-03 00:02:12 +0700

    Add babel preset-env

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,16 @@
 module.exports = {
   presets: [
     [
+      "@babel/preset-env",
+      {
+        useBuiltIns: "entry", // Automatically include polyfills based on usage
+        corejs: "3", // Use the latest version of core-js
+        targets: {
+          browsers: ["> 0.25%", "not dead", "Chrome 41"], // Specify older browser support
+        },
+      },
+    ],
+    [
       "@vue/cli-plugin-babel/preset",
       {
         useBuiltIns: "entry", // Automatically include polyfills
