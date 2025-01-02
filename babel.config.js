821c063d640ef43fd274865b66b538cb1034d211
Darren <darrenlionardo@gmail.com>
2025-01-02 23:05:45 +0700

    Adjust and fix babel and dependecies

--- babel.config.js
+++ babel.config.js
@@ -1,23 +1,14 @@
 module.exports = {
-  // presets: ["@vue/cli-plugin-babel/preset"],
   presets: [
     [
       "@babel/preset-env",
       {
-        useBuiltIns: "entry",
-        corejs: "3.22",
+        targets: {
+          chrome: "41",
+        },
+        useBuiltIns: "usage", // Automatically includes required polyfills
+        corejs: "3",
       },
     ],
-  ],
-  plugins: [
-    "@babel/plugin-transform-private-methods",
-    [
-      "transform-es2015-template-literals",
-      {
-        loose: true,
-        retainLines: true,
-      },
-    ],
-    "transform-es2015-arrow-functions",
   ],
 };
