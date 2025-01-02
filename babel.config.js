3260bedb4c2f8cac165c23245a34ed8c7f383a08
Darren <darrenlionardo@gmail.com>
2025-01-03 00:14:52 +0700

    Add additional babel plugins

--- babel.config.js
+++ babel.config.js
@@ -1,21 +1,19 @@
 module.exports = {
   presets: [
     [
-      "@babel/preset-env",
+      "@vue/cli-plugin-babel/preset",
       {
-        useBuiltIns: "entry", // Automatically include polyfills based on usage
+        useBuiltIns: "entry", // Automatically include polyfills
         corejs: "3", // Use the latest version of core-js
         targets: {
           browsers: ["> 0.25%", "not dead", "Chrome 41"], // Specify older browser support
         },
       },
     ],
-    [
-      "@vue/cli-plugin-babel/preset",
-      {
-        useBuiltIns: "entry", // Automatically include polyfills
-        corejs: "3", // Use the latest version of core-js
-      },
-    ],
+  ],
+  plugins: [
+    // Plugin to transform class properties and other modern syntax
+    "@babel/plugin-proposal-class-properties",
+    "@babel/plugin-transform-runtime", // For helper functions and async/await
   ],
 };
