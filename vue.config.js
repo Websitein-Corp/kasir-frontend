d9ee2a0e3a3ddc2fe2b8cfb05834de3c4c84468a
Darren <darrenlionardo@gmail.com>
2025-01-03 00:38:29 +0700

    adjust vue config

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  transpileDependencies: ["webpack-dev-server"],
+  transpileDependencies: [/node_modules/],
 });
